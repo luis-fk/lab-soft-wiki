0d033023b0941d994fed17cca75c1b5bcfc2c31e
C-Tadaki-S <103610912+C-Tadaki-S@users.noreply.github.com>
2024-09-21 15:54:42 -0300

    iniciando as tabelas do db

--- encyclopedia/models.py
+++ encyclopedia/models.py
@@ -14,49 +14,49 @@ class User(AbstractUser):
     def __str__(self):
         return self.name
 
-# class Endereco(models.Model):
-#     cidade = models.CharField(max_length=255)
-#     bairro = models.CharField(max_length=255)
-#     rua = models.CharField(max_length=255)
-#     numero = models.IntegerField()
-#     complemento = models.CharField(max_length=255, blank=True, null=True)
+class Endereco(models.Model):
+    cidade = models.CharField(max_length=255)
+    bairro = models.CharField(max_length=255)
+    rua = models.CharField(max_length=255)
+    numero = models.IntegerField()
+    complemento = models.CharField(max_length=255, blank=True, null=True)
 
-#     def __str__(self):
-#         return f"{self.rua}, {self.numero}, {self.cidade}"
+    def __str__(self):
+        return f"{self.rua}, {self.numero}, {self.cidade}"
 
-# class Denuncia(models.Model):
-#     title = models.CharField(max_length=255)
-#     text = models.TextField()
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+class Denuncia(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
 
-# class Historico(models.Model):
-#     num_changes = models.IntegerField()
-#     text_changes = models.TextField()
-#     edited_by = models.ForeignKey(User, on_delete=models.CASCADE)
-#     article = models.ForeignKey('Artigo', on_delete=models.CASCADE)
+class Historico(models.Model):
+    num_changes = models.IntegerField()
+    text_changes = models.TextField()
+    edited_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey('Artigo', on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return f"History of Article {self.article.title}"
+    def __str__(self):
+        return f"History of Article {self.article.title}"
 
-# class Artigo(models.Model):
-#     title = models.CharField(max_length=255)
-#     text = models.TextField()
-#     views = models.IntegerField(default=0)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Artigo(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    views = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
 
-# class Comentario(models.Model):
-#     text = models.TextField()
-#     likes = models.IntegerField(default=0)
-#     edited = models.BooleanField(default=False)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     article = models.ForeignKey(Artigo, on_delete=models.CASCADE)
+class Comentario(models.Model):
+    text = models.TextField()
+    likes = models.IntegerField(default=0)
+    edited = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Artigo, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return f"Comment on {self.article.title} by {self.user.name}"
+    def __str__(self):
+        return f"Comment on {self.article.title} by {self.user.name}"
