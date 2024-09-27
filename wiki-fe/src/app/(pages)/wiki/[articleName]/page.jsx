b8bed7c64f3274e9a3bba92f6ba765397f160a71
fl-korbes <felipekorbes@live.com>
2024-09-27 16:03:25 -0300

    pequenas mudanças

--- wiki-fe/src/app/(pages)/wiki/[articleName]/page.jsx
+++ wiki-fe/src/app/(pages)/wiki/[articleName]/page.jsx
@@ -3,16 +3,16 @@ import dengue from '@/assets/json/dengue.json'
 import Content from '@/components/Content'
 
 export default async function page({params}) {
-    let data = await fetch('http://127.0.0.1:8000/random/')
-    let posts = await data.json()
-
     if (params.articleName === "dengue") {
+        let data = await fetch('http://127.0.0.1:8000/random')
+        let posts = await data.json()
+
         return (
             <Content params={{title: posts[0].title, content: posts[0].text}}/>
         )
     }
 
     return (
-    <Content params={{title: "Artigo vazio", content: "Nada a mostrar"}}/>
+        <Content params={{title: "Artigo vazio", content: "Nada a mostrar"}}/>
     )
 }
--- wiki-fe/src/app/_components/Content.jsx
+++ wiki-fe/src/app/_components/Content.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import '@/styles/content.css'
 
 export default function Content({params}) {
-
   return (
     <div className='content-container'>
       <h1>{params?.title}</h1>
