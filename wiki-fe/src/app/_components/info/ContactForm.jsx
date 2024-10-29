c26a3deecef65375840a37ca6fdc83d72cb81d8d
fl-korbes <felipekorbes@live.com>
2024-10-29 17:10:04 -0300

    get in contact page

---
+++ wiki-fe/src/app/_components/info/ContactForm.jsx
@@ -0,0 +1,80 @@
+"use client";
+import "@/styles/info/contact-form.css";
+import ErrorMessage from '@/components/auth/ErrorMessage';
+import SuccessMessage from '@/components/auth/SuccessMessage';
+import React, { useState, useEffect } from 'react';
+
+export default function ContactForm() {
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [successMessage, setSuccessMessage] = useState(null);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            if (errorMessage) setErrorMessage(null);
+            if (successMessage) setSuccessMessage(null);
+        }, 5000);
+
+        return () => clearTimeout(timeout);
+    }, [errorMessage, successMessage]);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        
+        try {
+            const formData = new FormData(event.target);
+
+            const data = {
+                subject: formData.get('subject'),
+                email: formData.get('email'),
+                content: formData.get('content')
+            };
+
+            const response = await fetch('/api/email', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            const message = await response.json();
+            if (!response.ok || message.message.includes("Ocorreu algum erro")) {
+                setErrorMessage(message.message);
+                return;
+            }
+            
+            setSuccessMessage(message.message);
+        } catch (error) {
+            setErrorMessage("Ocorreu um erro ao enviar o email, por favor tente novamente mais tarde!");
+        }
+    };
+
+    return (
+        <div className="contact-container">
+            <h2>Entre em contato</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="subject">Assunto</label>
+                    <input type="text" name="subject" placeholder="Assunto" required />
+                </div>
+
+                <div>
+                    <label htmlFor="email">Seu email:</label>
+                    <input type="email" name="email" placeholder="Email" required />
+                </div>
+
+                <div>
+                    <label htmlFor="content">Conteúdo</label>
+                    <textarea name="content" placeholder="Deixe sua mensagem aqui..." rows="5" required></textarea>
+                </div>
+
+                <div className="submitButton-container">
+                    <button type="submit">Enviar Email</button>
+                </div>
+
+                {errorMessage && <ErrorMessage message={errorMessage} />}
+                {successMessage && <SuccessMessage message={successMessage} />}
+            </form>
+        </div>
+    );
+}
